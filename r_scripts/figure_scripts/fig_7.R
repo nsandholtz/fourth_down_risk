c800746ba965939fcb0da50bf9f4aa615167ceed
Nathan Sandholtz <nsandholtz@stat.byu.edu>
2023-09-01 12:01:56 -0600

    Unneeded comments

--- r_scripts/figure_scripts/fig_7.R
+++ r_scripts/figure_scripts/fig_7.R
@@ -32,21 +32,18 @@ tau_hat_seas_plot <- tau_hat_seas_plot_df |>
   ggplot(aes(x = tau, y = season, weight = weight, fill = field_region)) +
   geom_boxplot(
     outlier.size = .5
-    #outlier.shape = NA
     ) +
   facet_grid(field_region~wp_group, 
              labeller = label_bquote(cols = "WP Group =" ~ .(wp_group))) +  
   scale_fill_manual(name = "Field Region",
                     values = c(rgb(1,.65,0,.5),
                                rgb(0,0,.5,.5))) +
-  #scale_y_discrete(labels= function(x) highlight(x, "4th Down Bot", "black")) +
   scale_x_continuous(limits=c(.2,.8), breaks = seq(.2, .8, by = .1)) +
   ylab("") +
   xlab(expression(tau)) + 
   theme_bw() +
   theme(strip.background.x = element_rect(colour = 'black', fill = NA),
         strip.background.y = element_rect(colour = 'black', fill = NA),
-        #axis.text.y = element_markdown(size = 9)
         ) +
   guides(fill = "none")
 
